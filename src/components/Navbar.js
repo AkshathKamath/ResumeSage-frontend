70d9901f56e58cf7b61643eb46e35092490e0f65
AkshathKamath <akshathkamathwork@gmail.com>
2024-08-24 21:10:16 +0530

    Modified Title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <NavbarBrand href="/" className="mx-4">
-        <img src="./ResumeSage_logo.png" className="logo mx-2"></img>
-        ResumeSage
+        <img src="./jobsage_logo.jpg" className="logo mx-2"></img>
+        JobSage
       </NavbarBrand>
       <NavbarBrand href="/view" className="fs-6 mx-10">
         View
